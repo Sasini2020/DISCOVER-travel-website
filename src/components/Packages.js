6caea25f7dd28e08de87ef23727b49d220baca01
Sasini <63692107+Sasini2020@users.noreply.github.com>
2021-07-20 20:47:08 +0530

    Created new package cards

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,51 +1,56 @@
 import React from 'react';
 import './Cards.css';
 import './PackageCards.css';
-import { Link } from 'react-router-dom';
 
 function Packages() {
   return (
     <>
     <br></br><br></br>
-    <h1>What Our Clients Say</h1>
+    <h1>WHAT OUR CLIENTS SAY</h1><br></br><br></br>
     <div class="card-container">
-      <div class="card card-1">
-        <div class="card__icon"><i class="fas fa-bolt"></i></div>
-        <p class="card__exit"><i class="fas fa-times"></i></p>
-        <h2 class="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
+    <div class="card card-1">
+        <div class="card__icon"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></div>
+        <p class="card__exit"><i class="far fa-bookmark"></i></p>
+        <h2 class="card__title">John K.</h2>
+        <p class="card__apply">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
-      <div class="card card-2">
-        <div class="card__icon"><i class="fas fa-bolt"></i></div>
-        <p class="card__exit"><i class="fas fa-times"></i></p>
-        <h2 class="card__title">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
-      </div>
-      <div class="card card-3">
-        <div class="card__icon"><i class="fas fa-bolt"></i></div>
-        <p class="card__exit"><i class="fas fa-times"></i></p>
-        <h2 class="card__title">Ut enim ad minim veniam.</h2>
-        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
-      </div>
+
       <div class="card card-4">
-        <div class="card__icon"><i class="fas fa-bolt"></i></div>
-        <p class="card__exit"><i class="fas fa-times"></i></p>
-        <h2 class="card__title">Quis nostrud exercitation ullamco laboris nisi.</h2>
-        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
+        <div class="card__icon"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></div>
+        <p class="card__exit"><i class="far fa-bookmark"></i></p>
+        <h2 class="card__title">Kithnuka Yuhas</h2>
+        <p class="card__apply">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
-      <div class="card card-5">
-        <div class="card__icon"><i class="fas fa-bolt"></i></div>
-        <p class="card__exit"><i class="fas fa-times"></i></p>
-        <h2 class="card__title">Ut aliquip ex ea commodo consequat. Duis aute irure dolor.</h2>
-        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
+
+      <div class="card card-3">
+        <div class="card__icon"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></div>
+        <p class="card__exit"><i class="far fa-bookmark"></i></p>
+        <h2 class="card__title">Simaya Jodhi</h2>
+        <p class="card__apply">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
+
+      <div class="card card-4">
+        <div class="card__icon"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></div>
+        <p class="card__exit"><i class="far fa-bookmark"></i></p>
+        <h2 class="card__title">Peter J.</h2>
+        <p class="card__apply">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+
       <div class="card card-1">
-        <div class="card__icon"><i class="fas fa-bolt"></i></div>
-        <p class="card__exit"><i class="fas fa-times"></i></p>
-        <h2 class="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
+        <div class="card__icon"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></div>
+        <p class="card__exit"><i class="far fa-bookmark"></i></p>
+        <h2 class="card__title">Kithnuka Yuhas</h2>
+        <p class="card__apply">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+
+      <div class="card card-6">
+        <div class="card__icon"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></div>
+        <p class="card__exit"><i class="far fa-bookmark"></i></p>
+        <h2 class="card__title">Kevin Shaz</h2>
+        <p class="card__apply">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
     </div>
+    <br></br><br></br><br></br><br></br>
   
 </>
   );
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../../App.js';
 import Footer from '../Footer.js';
 import Offers from '../Offers.js';
-import Packages from '../Packages.js';
 
 export default function Services() {
     return (
