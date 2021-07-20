2bb8e8cfb08e943e63de8bcd2a1e0c8a94e7d4d1
Sasini <63692107+Sasini2020@users.noreply.github.com>
2021-07-20 18:11:38 +0530

    Added new compoents

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,56 +1,53 @@
 import React from 'react';
 import './Cards.css';
-import CardItem from './CardItem';
+import './PackageCards.css';
+import { Link } from 'react-router-dom';
 
 function Packages() {
   return (
-    <div className='cards'>
-      <h1> POPULAR PACKAGES</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/road-trip-with-raj-0uI8LANIe0A-unsplash.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
-            />
-            <CardItem
-              src='images/farhath-firows-vo0mwCg5FJc-unsplash.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
-            {/* <CardItem
-              src='images/sigiriya.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            /> */}
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/shainee-fernando-WDVMyBsSSwY-unsplash.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
-            />
-            <CardItem
-              src='images/eddy-billard-d6EuGZs41SI-unsplash.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
-            />
-            <CardItem
-              src='images/gemmmm-BS8a67PahbM-unsplash.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
-            />
-          </ul>
-        </div>
+    <>
+    <br></br><br></br>
+    <h1>What Our Clients Say</h1>
+    <div class="card-container">
+      <div class="card card-1">
+        <div class="card__icon"><i class="fas fa-bolt"></i></div>
+        <p class="card__exit"><i class="fas fa-times"></i></p>
+        <h2 class="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
+      </div>
+      <div class="card card-2">
+        <div class="card__icon"><i class="fas fa-bolt"></i></div>
+        <p class="card__exit"><i class="fas fa-times"></i></p>
+        <h2 class="card__title">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
+      </div>
+      <div class="card card-3">
+        <div class="card__icon"><i class="fas fa-bolt"></i></div>
+        <p class="card__exit"><i class="fas fa-times"></i></p>
+        <h2 class="card__title">Ut enim ad minim veniam.</h2>
+        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
+      </div>
+      <div class="card card-4">
+        <div class="card__icon"><i class="fas fa-bolt"></i></div>
+        <p class="card__exit"><i class="fas fa-times"></i></p>
+        <h2 class="card__title">Quis nostrud exercitation ullamco laboris nisi.</h2>
+        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
+      </div>
+      <div class="card card-5">
+        <div class="card__icon"><i class="fas fa-bolt"></i></div>
+        <p class="card__exit"><i class="fas fa-times"></i></p>
+        <h2 class="card__title">Ut aliquip ex ea commodo consequat. Duis aute irure dolor.</h2>
+        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
+      </div>
+      <div class="card card-1">
+        <div class="card__icon"><i class="fas fa-bolt"></i></div>
+        <p class="card__exit"><i class="fas fa-times"></i></p>
+        <h2 class="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+        <p class="card__apply"><Link to='/' className='link'>Get Now <i class="fas fa-arrow-right"></i></Link></p>
       </div>
     </div>
+  
+</>
   );
 }
 
--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../../App.css';
 import AboutContent from '../AboutContent';
 import Footer from '../Footer';
+import Packages from '../Packages';
 
 export default function Products() {
   return(
     <>
     <AboutContent />
+    <Packages />
     <Footer />
   </>
   );
